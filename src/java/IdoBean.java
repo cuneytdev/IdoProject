b498b839a9fcf9ae4824a5c33a654e2d45da69f6
cuneytdalan <cuneytdalan17@gmail.com>
2016-04-29 20:14:33 +0300

    Added files via upload

---
+++ src/java/IdoBean.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author cuney
+ */
+@ManagedBean
+@RequestScoped
+public class IdoBean {
+
+    /**
+     * Creates a new instance of IdoBean
+     */
+    public IdoBean() {
+    }
+    
+}
